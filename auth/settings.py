5a75afed9bd833994463a03c6d796d072744b5a5
Mikhail Podgurskiy <kmmbvnr@gmail.com>
2016-03-17 14:04:36 +0600

    Fixdeploy

--- auth/settings.py
+++ auth/settings.py
@@ -15,8 +15,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
-    'material',
-    'material.admin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
