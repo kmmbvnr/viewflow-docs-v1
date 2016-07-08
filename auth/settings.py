e80b2a506437f2789fda5ea8308c003379e447e9
Mikhail Podgurskiy <kmmbvnr@gmail.com>
2016-07-08 11:33:15 +0600

    Update deps

--- auth/settings.py
+++ auth/settings.py
@@ -15,6 +15,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
+    'material',
+    'material.admin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
